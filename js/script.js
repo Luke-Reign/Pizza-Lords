34eba30513e09a621fe17d1718f7a5f2ff75e500
Kibiku3 <lkibiku@gmail.com>
2019-07-28 23:31:33 +0300

    Make the functioning of the buttons place your order

--- js/script.js
+++ js/script.js
@@ -22,3 +22,28 @@ var timesThree=function(number1,number4){
                 var number3=parseInt("900");
                 var number4=parseInt("1100");
             
+                if (document.getElementById('small').checked){
+                    var result1=timesOne(number1,number2);
+                    $("#result").text("Your total amount is: Ksh."+result1+".");
+                 
+                }
+                else if (document.getElementById('medium').checked){
+                var result2=timesTwo(number1,number3);
+                $("#result").text("Your total amount is: Ksh."+result2+".");
+                   }
+                else if(document.getElementById('large').checked){
+                var result3=timesThree(number1,number4);
+                $("#result").text("Your total amount is: Ksh."+result3+ ".");
+                   }
+                else{
+                    alert('Error! Some fields are empty');
+                   }
+                });
+                
+                $("form#delivery").submit(function(event){
+                    event.preventDefault();
+                    alert("Your Order will be delivered to your location");
+                    alert("The delivery cost is Ksh.200")
+                });
+                
+                });
