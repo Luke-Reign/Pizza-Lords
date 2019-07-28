18054c6ce83c3265423d3035031530760fa6524c
Kibiku3 <lkibiku@gmail.com>
2019-07-28 23:28:55 +0300

    Create a function for the number box

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+  var timesOne=function(number1,number2){
+    return number1*number2;
+    };
+var timesTwo=function(number1,number3){
+        return number1*number3;
+        };
+var timesThree=function(number1,number4){
+            return number1*number4;
+            };
+
