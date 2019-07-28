cf996fede206352564e803150fcd7bd5b2c01c2d
Kibiku3 <lkibiku@gmail.com>
2019-07-28 23:30:16 +0300

    Create a toggle effect on the sub-menus

--- js/script.js
+++ js/script.js
@@ -9,3 +9,16 @@ var timesThree=function(number1,number4){
             return number1*number4;
             };
 
+
+            $(document).ready(function() {
+                $(".tablehead").click(function(){
+                    $(this).next("div").slideToggle("slow");
+                });
+                
+               $("form#total").submit(function(event){
+                event.preventDefault();
+                var number1=parseInt($("#timesNum").val());
+                var number2=parseInt("650");
+                var number3=parseInt("900");
+                var number4=parseInt("1100");
+            
